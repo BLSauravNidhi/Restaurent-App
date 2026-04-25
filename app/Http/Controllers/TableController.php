d33c1e9ebccf73f21db1c979c65c3ec48ada308c
saurav <sauravkumarverma92@gmail.com>
2026-04-25 12:29:50 +0530

    Timezone setup and checked the table is active or not.

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+// use App\Models\Table;
+use App\Models\TableSession;
+
 // use Illuminate\Http\Request;
 
 class TableController extends Controller
 {
-    public function scanQR(){
-        return '<h1> scanned </h1>';
+    public function GetTableAccess(string $tableId){
+        // check status 
+        $hasSession = TableSession::where('table_id', $tableId)
+            ->where('active', true)
+            ->where('expires_at', '>', now())->first();
+
+        if($hasSession){
+            return $hasSession;
+        } else {
+            return "waiting for approval";
+        }
     }
 }
