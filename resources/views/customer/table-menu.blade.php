383c17cebd6ea402d96e646c4c5a3b31f78bfa00
saurav <sauravkumarverma92@gmail.com>
2026-05-30 12:59:37 +0530

    Removed useless comments.

--- resources/views/customer/table-menu.blade.php
+++ resources/views/customer/table-menu.blade.php
@@ -119,78 +119,10 @@
                                         </svg>
                                     </button>
                                 @endif
-                                {{-- <svg height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/></svg> --}}
-
-                                {{-- <svg height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff"><path d="M440-120v-320H120v-80h320v-320h80v320h320v80H520v320h-80Z"/></svg> --}}
                     </span>
                 </div>
             </div>
             @endforeach
-
-
-            {{-- <div class="w-90 h-32 shrink- rounded-2xl bg-gray-200 overflow-hidden relative grid grid-cols-[35%_auto_25%]">
-                <span class=" absolute top-0 left-0 bg-black text-white rounded-br-2xl px-2 py-1.5 text-xs font-medium rubik">34% off</span>
-                <div class="flex justify-end items-center">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTthMItWzAAXDjKOuUNiN3b1me3pSweyIx7fA&s" alt="" class=" scale-75 w-38 mix-blend-multiply aspect-square">
-                </div>
-                <div class="details py-3 flex flex-col gap-0.5">
-                    <h2 class="text-black font-bold inter capitalize">Cheese Burger</h2>
-                    <p class="text-sm text-gray-600">Offer vailid today only</p>
-                    <span class=" w-fit bg-orange-600 px-2 py-0.5 rounded-md font-medium text-sm text-white rubik">Offer</span>
-                    <p class=" font-bold ">₹148</p>
-                </div>
-                <div class=" fill-black flex flex-col items-end justify-between p-3">
-                    <svg viewBox="0 -960 960 960" class=" w-7 h-7 bg-white rounded-3xl p-1"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-
-                    <span cart-id="cart-id" item-id="1" class=" cart-item w-fit h-fit flex flex-nowrap justify-center items-center text-center bg-white rounded-2xl font-bold overflow-hidden">
-                        <button data-action="add-to-cart" class=" btn-update">+</button>
-                    </span>
-                </div>
-            </div> --}}
-
-            {{-- <div class="w-90 h-32 shrink- rounded-2xl bg-gray-200 overflow-hidden relative grid grid-cols-[35%_auto_25%]">
-                <span class=" absolute top-0 left-0 bg-black text-white rounded-br-2xl px-2 py-1.5 text-xs font-medium rubik">34% off</span>
-                <div class="flex justify-end items-center">
-                    <img src="https://img.magnific.com/free-psd/top-view-delicious-pizza_23-2151868964.jpg?semt=ais_hybrid&w=740&q=80" alt="" class=" scale-75 w-38 mix-blend-multiply aspect-square">
-                </div>
-                <div class="details py-3 flex flex-col gap-0.5">
-                    <h2 class="text-black font-bold inter capitalize">Pizza Hut Italian</h2>
-                    <p class="text-sm text-gray-600">Offer vailid today only</p>
-                    <span class=" w-fit bg-orange-600 px-2 py-0.5 rounded-md font-medium text-sm text-white rubik">Offer</span>
-                    <p class=" font-bold ">₹239</p>
-                </div>
-                <div class=" fill-black flex flex-col items-end justify-between p-3">
-                    <svg viewBox="0 -960 960 960" class=" w-7 h-7 bg-white rounded-3xl p-1"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-
-                    <span class=" w-full h-fit grid grid-cols-3 justify-center items-center text-center bg-white rounded-2xl font-bold overflow-hidden">
-                        <button data-action="decreament">-</button>
-                        <span class="quantity">1</span>
-                        <button data-action="increament">+</button>
-                    </span>
-                </div>
-            </div>
-
-            <div class="w-90 h-32 shrink- rounded-2xl bg-gray-200 overflow-hidden relative grid grid-cols-[35%_auto_25%]">
-                <span class=" absolute top-0 left-0 bg-black text-white rounded-br-2xl px-2 py-1.5 text-xs font-medium rubik">34% off</span>
-                <div class="flex justify-end items-center">
-                    <img src="https://static.vecteezy.com/system/resources/previews/052/644/653/non_2x/delicious-buttered-lobster-platter-for-gourmet-dining-on-transparent-background-png.png" alt="" class=" scale-75 w-38 mix-blend-multiply aspect-square">
-                </div>
-                <div class="details py-3 flex flex-col gap-0.5">
-                    <h2 class="text-black font-bold inter capitalize">Lobster</h2>
-                    <p class="text-sm text-gray-600">Offer vailid today only</p>
-                    <span class=" w-fit bg-orange-600 px-2 py-0.5 rounded-md font-medium text-sm text-white rubik">Offer</span>
-                    <p class=" font-bold ">₹899</p>
-                </div>
-                <div class=" fill-black flex flex-col items-end justify-between p-3">
-                    <svg viewBox="0 -960 960 960" class=" w-7 h-7 bg-white rounded-3xl p-1"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-
-                    <span class=" w-full h-fit grid grid-cols-3 justify-center items-center text-center bg-white rounded-2xl font-bold overflow-hidden">
-                        <button data-action="decreament">-</button>
-                        <span class="quantity">2</span>
-                        <button data-action="increament">+</button>
-                    </span>
-                </div>
-            </div> --}}
 
             {{-- Items End  --}}
         </div>
